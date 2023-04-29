19ad392391a5dd8d36539952f7262bc07baab0ca
heshgggg <87483961+heshgggg@users.noreply.github.com>
2023-04-29 15:07:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function () {
+  const preloader = document.querySelector(".preloader");
+  preloader.classList.add("done");
+});
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
